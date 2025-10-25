62133b10a4c5824b531e9279a9a30def0abe77cf
epepei <manya.ermakova@gmail.com>
2025-10-25 12:50:41 +0300

    Update rectangle.h

--- rectangle.h
+++ rectangle.h
@@ -4,25 +4,18 @@
 class Rectangle : public Figure {
 private:
     Point vertices[4];
-
 public:
     Rectangle() = default;
     Rectangle(const Point& p1, const Point& p2, const Point& p3, const Point& p4);
     Rectangle(const Rectangle& other);
-    
     Point geometricCenter() const override;
     double area() const override;
     void printVertices(std::ostream& os) const override;
     void readFromStream(std::istream& is) override;
-    
-
     std::unique_ptr<Figure> clone() const override;
-    
     Rectangle& operator=(const Rectangle& other);
     Rectangle& operator=(Rectangle&& other) noexcept;
     bool operator==(const Rectangle& other) const;
-    
-    
     friend std::ostream& operator<<(std::ostream& os, const Rectangle& rectangle);
     friend std::istream& operator>>(std::istream& is, Rectangle& rectangle);
 };
