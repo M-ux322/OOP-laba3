533e07432039e7eced0cee58d6a1ed041598a4fa
epepei <manya.ermakova@gmail.com>
2025-10-25 12:47:12 +0300

    Update figure.cpp

--- figure.cpp
+++ figure.cpp
@@ -1,10 +1,8 @@
 #include "Figure.h"
 
-
 bool Figure::operator==(const Figure& other) const {
     return this->area() == other.area();
 }
-
 Figure::operator double() const {
     return area();
 }
