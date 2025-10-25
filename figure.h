1baa76b4b1d8eb6870ebd5a0d8e8410d2904f552
epepei <manya.ermakova@gmail.com>
2025-10-25 12:47:48 +0300

    Update figure.h

--- figure.h
+++ figure.h
@@ -3,18 +3,13 @@
 #include <memory>
 
 class Figure {
-    
 public:
-
     virtual ~Figure() = default;
-    
     virtual Point geometricCenter() const = 0;
     virtual double area() const = 0;
     virtual void printVertices(std::ostream& os) const = 0;
     virtual void readFromStream(std::istream& is) = 0;
-    
     virtual std::unique_ptr<Figure> clone() const = 0;
-    
     bool operator==(const Figure& other) const;
     operator double() const;
 };
